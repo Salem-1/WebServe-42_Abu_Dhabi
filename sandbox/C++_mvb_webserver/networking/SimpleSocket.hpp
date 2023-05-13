4140ee804a6ee3ba5decdaa2334fe5d1621c9b65
Salem-1 <ph.ahmedsalem@gmail.com>
2023-05-13 16:57:22 +0400

    add initial webserve file tree

---
+++ sandbox/C++_mvb_webserver/networking/SimpleSocket.hpp
@@ -0,0 +1,32 @@
+#ifndef SIMPLESOCKET_HPP
+# define SIMPLESOCKET_HPP
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <iostream>
+//domain  --> IP
+//service --> socket type
+//protocol -> TCP/IP UDP ..
+namespace ft
+{
+    class SimpleSocket
+    {
+        private:
+            struct sockaddr_in  address;
+            int                 sock;
+            int                 connection;
+        public:
+            SimpleSocket(
+                int domain, int service, int protocol, int port, u_long interface);            
+            ~SimpleSocket();
+            virtual int         connect_to_network_as_bind_or_connect(int socke, 
+                                struct sockaddr_in address) = 0;
+            struct sockaddr_in  get_address();
+            void                test_connection(int);
+            int                 get_sock();
+            int                 get_connection();
+            void                set_address(struct sockaddr_in  given_address);
+            void                set_sock(int  input_socket);
+            void                set_connection(int input_connection);
+    };
+};
+#endif
