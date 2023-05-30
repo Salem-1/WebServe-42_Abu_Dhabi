cbc70865e65023e873263f2258fd88910410b55c
Salem-1 <ph.ahmedsalem@gmail.com>
2023-05-30 12:17:10 +0400

    after reading 9112 rfc

---
+++ sandbox/try_get_protobyname/kqueue_server.hpp
@@ -0,0 +1,30 @@
+#ifndef KQUEUE_SERVER_HPP
+# define KQUEUE_SERVER_HPP
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/socket.h>
+# include <sys/event.h>
+# include <netinet/in.h>
+# include <fcntl.h>
+#include <netdb.h>
+# define MAX_EVENTS 1000
+# define BUFFER_SIZE 1024
+
+typedef struct server_vars 
+{
+    int                 server_socket;
+    int                 client_socket;
+    int                 kq;
+    int                 n;
+    int                 fd;
+    int                 filter;
+    struct kevent       event;
+    struct kevent       events[MAX_EVENTS];
+    struct sockaddr_in  server_address;
+    struct sockaddr_in  client_address;
+    socklen_t client_address_len;
+}   server_vars;
+
+#endif
