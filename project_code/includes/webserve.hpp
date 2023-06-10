1c774ea893658ab6ab5e4d6dd07cf6550cf7c21c
Salem-1 <ph.ahmedsalem@gmail.com>
2023-06-10 20:40:06 +0400

    adding blue print of parsing

--- project_code/includes/webserve.hpp
+++ project_code/includes/webserve.hpp
@@ -6,5 +6,6 @@
 # include "../srcs/server/Kque.hpp"
 # include "../srcs/client/Client.hpp"
 # include "../srcs/recive_request/Receive.hpp"
+# include "../srcs/recive_request/Parser.hpp"
 
 #endif
--- project_code/srcs/recive_request/Receive.cpp
+++ project_code/srcs/recive_request/Receive.cpp
@@ -28,16 +28,26 @@ Receive::~Receive()
 
 void    Receive::receive_all()
 {
-    bytes_read = recv(read_sock, buffer, BUFFER_SIZE, 0);
-    //all parsing in the program happens here inshalla
-    
     //parsing ends
+    parser(buffer);
+    read_packet(buffer);
     if (bytes_read == 0)
         state = KILL_CONNECTION;
     else
     {
+        //here should have the packet perfect parsed for preparing the response
         printf("recieved data from client\n");
         printf("%.*s/n", (int)bytes_read, buffer);
     }
 }
 
+
+void    Receive::read_packet(char *buffer)
+{
+    bytes_read = recv(read_sock, buffer, BUFFER_SIZE, 0);
+    //all parsing in the program happens here inshalla
+    parser.parse(buffer);
+    if(parser.read_again)
+        read_packet(buffer);
+    bytes_read = parser.bytes_read;
+}
