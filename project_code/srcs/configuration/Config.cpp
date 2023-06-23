86d64cc5c6ea108fdf5d1bfc1964083bc15bd67d
Salem-1 <ph.ahmedsalem@gmail.com>
2023-06-23 20:43:59 +0400

    done multi-threading

--- project_code/srcs/configuration/Config.cpp
+++ project_code/srcs/configuration/Config.cpp
@@ -29,6 +29,33 @@ Config::Config()
     servers[0]["/styles index"] = servers[0]["root"] + "/styles/" + "styles.css";
     servers[0]["/js"] = servers[0]["root"] + "/js/";
     servers[0]["/js index"] = servers[0]["root"] + "/js/" + "script.js";
+   
+   
+    servers.push_back(std::map<std::string, std::string>());
+    servers[1]["server_name"] = "127.0.0.1";
+    servers[1]["Port"] = "4444";
+    servers[1]["root"] = "/Users/ahsalem/projects/cursus/webserve/project_code/testers/our_tester/intra/YoupiBanane";
+    servers[1]["index"] = "youpi.bad_extension";
+    servers[1]["/"] = servers[1]["root"] + "/" + servers[1] ["index"];
+    servers[1]["/images"] = servers[1]["root"] + "/path_to_images/";
+    servers[1]["/images index"] = servers[1]["/images"] + "base_image.jpg";
+    servers[1]["/styles"] = servers[1]["root"] + "/styles/";
+    servers[1]["/styles index"] = servers[1]["root"] + "/styles/" + "styles.css";
+    servers[1]["/js"] = servers[1]["root"] + "/js/";
+    servers[1]["/js index"] = servers[1]["root"] + "/js/" + "script.js";
+   
+    servers.push_back(std::map<std::string, std::string>());
+    servers[2]["server_name"] = "127.0.0.1";
+    servers[2]["Port"] = "5555";
+    servers[2]["root"] = "/Users/ahsalem/projects/cursus/webserve/project_code/testers/our_tester/website";
+    servers[2]["index"] = "index.html";
+    servers[2]["/"] = servers[2]["root"] + "/" + servers[2] ["index"];
+    servers[2]["/images"] = servers[2]["root"] + "/path_to_images/";
+    servers[2]["/images index"] = servers[2]["/images"] + "base_image.jpg";
+    servers[2]["/styles"] = servers[2]["root"] + "/styles/";
+    servers[2]["/styles index"] = servers[2]["root"] + "/styles/" + "styles.css";
+    servers[2]["/js"] = servers[2]["root"] + "/js/";
+    servers[2]["/js index"] = servers[2]["root"] + "/js/" + "script.js";
     
     // servers.push_back(std::map<std::string, std::string>());
     // servers[2]["server_name"] = "127.0.0.1";
