de9bba4c0bbdacce7431bc3af6c6ea7dc6b1b6fb
Ahmed Yassin <ayassin@lab1r4s8.42abudhabi.ae>
2023-07-04 09:25:08 +0400

    i have no idea what I am doing

--- project_code/srcs/main.cpp
+++ project_code/srcs/main.cpp
@@ -11,7 +11,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/webserve.hpp"
+#include "../includes/webserve.hpp"
 
 
 int main( )
