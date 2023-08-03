dba04906b3344f13ea6c5f321bb77b94ba50d110
Salem-1 <ph.ahmedsalem@gmail.com>
2023-08-03 23:29:49 +0400

    ignoring log dir

---
+++ sandbox/path_alog.cpp
@@ -0,0 +1,15 @@
+//bearkdown the given path to directory and the other part of the path
+//if it's a dir check
+            if it's not listed:
+                construct the path
+            else
+                throw to get
+                if_d
+
+
+
+/*
+ask about the impact on global warming?
+ask about the waste in electricity and computational power?
+ask about the money laundary?
+*/
