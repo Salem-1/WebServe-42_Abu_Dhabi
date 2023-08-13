aad06a84c6f5701751f2c55a5c6b282d5e8d39c2
Salem-1 <ph.ahmedsalem@gmail.com>
2023-08-13 09:41:11 +0400

    commiting before branching conf

---
+++ documentation/conf_workflow.cpp
@@ -0,0 +1,34 @@
+//Bim Ellah elrahman Elraheem
+
+the breakdown of the conf file as follows:
+Yassin
+         |
+         V 
+[server1 , server2, server3]
+    /    \
+    |     \
+essentials \
+            \
+        [location1, location, location3]
+---------------------------------------------- > Ahmed mahdy -> list of essentials, conf file with all options
+            |
+            |
+            V 
+        location check and break down
+
+
+
+Workflow:
+1- Take input (file.conf), output vector_servers< 
+                                                std::pair<
+                                                            std::string esentials,
+                                                            std::vector<std::string> locations>
+                                                          >
+                                                > 
+2-Ahmed Yassin, conf with all options 
+    list of errors.
+    list of options ---> essentials
+        |
+        v
+    optional
+3-Ahmed salem, take the 
