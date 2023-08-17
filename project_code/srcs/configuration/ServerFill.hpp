4e3392c7dd7973b694e1418de5e6fa3e1176b851
Salem-1 <ph.ahmedsalem@gmail.com>
2023-08-17 16:17:38 +0400

    have no Idea why the 3 port failed and now work

--- project_code/srcs/configuration/ServerFill.hpp
+++ project_code/srcs/configuration/ServerFill.hpp
@@ -8,27 +8,41 @@ class ServerFill
     public:
         ServerFill(tokenized_conf &conf_tokens);
         ~ServerFill();
-        bool                parseEssentials();
-        void                essentialsBasicCheck(
-                                std::string &row_essentials, std::vector<std::string> &essentials_vec);
-        void                fillEssentials(std::vector<std::string> &essentials);
-        void                replaceNlAndTabs(std::string &str);
-        void                fillPorts(std::vector<std::string> &listen_vec, 
-                                std::set<std::string> &essentials_arg);
-        int                 isAllDigit(std::string str);
-        void                fillServerNames(std::vector<std::string> &hosts_vec,  
-                                std::set<std::string> &essentials_arg, stringmap &server);
-        void                fill_essential_arg(std::set<std::string> &essential_arg);
-        void                fillRoot(std::vector<std::string> &root_vec,  std::set<std::string> &essentials_arg, 
-                                stringmap &server);
-        void                fillIndex(std::vector<std::string> &index_vec,  std::set<std::string> &essentials_arg, 
-                                stringmap &server);
-        void                fillBodySize(std::vector<std::string> &bodySize_vec,  std::set<std::string> &essentials_arg, 
-                                stringmap &server);
-        tokenized_conf          _conf_tokens;
-        Config                  servers;
-        std::vector<std::string> multiple_ports;
-        std::vector<std::string> multiple_index;
+        bool                    parseEssentials();
+        // std::vector<std::string> getMultiplePorts() const;
+        // std::vector<std::string> getMultipleindex() const;
+        // Config                      getServers();
+        tokenized_conf              _conf_tokens;
+        Config                      servers;
+        void                        essentialsBasicCheck(
+                                        std::string &row_essentials, std::vector<std::string> &essentials_vec);
+        void                        fillEssentials(std::vector<std::string> &essentials);
+        void                        replaceNlAndTabs(std::string &str);
+        void                        fillPorts(std::vector<std::string> &listen_vec, 
+                                        std::set<std::string> &essentials_arg);
+        int                         isAllDigit(std::string str);
+        void                        fillServerNames(std::vector<std::string> &hosts_vec,  
+                                        std::set<std::string> &essentials_arg, stringmap &server);
+        void                        fill_essential_arg(std::set<std::string> &essential_arg);
+        void                        fillRoot(std::vector<std::string> &root_vec,  std::set<std::string> &essentials_arg, 
+                                        stringmap &server);
+        void                        fillIndex(std::vector<std::string> &index_vec,  std::set<std::string> &essentials_arg, 
+                                        stringmap &server);
+        void                        fillBodySize(std::vector<std::string> &bodySize_vec,  std::set<std::string> &essentials_arg, 
+                                        stringmap &server);
+        void                        fillMultiplePorts(void);
+        void                        assignEssentials(
+                                        std::set<std::string> &essential_arg, std::vector<std::string> &essentials);
+        void                        flushSingleServer(void);
+        void                        checkRepeatedServers(void);
+        bool                        findRepeatedPort(conf::iterator &it, conf::iterator &repeated_it);
+        void                        checkDuplicateServerNames(conf::iterator &it, conf::iterator &repeated_it);
+        std::vector<std::string>    multiple_ports;
+        std::vector<std::string>    multiple_index;
+    private:
+        size_t                   i;
+        size_t                   port_counter;
+    
 };
 
 
